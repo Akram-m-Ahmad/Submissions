fa12895053ef532d67bf88e698b1b42e843c869a
Akram <akram.m.ahmad1989@gmail.com>
2019-12-16 20:50:48 +0200

    Step_4 Js2 Done

---
+++ Exercises/Javascript-Basics-02/step 4/Step_4.js
@@ -0,0 +1,9 @@
+function fun() {
+  if (confirm("Yes/No?")) {
+    document.getElementById("name").value = " ";
+    document.getElementById("surname").value = " ";
+    document.getElementById("city").value = " ";
+  } else {
+    alert("Love Codi");
+  }
+}
