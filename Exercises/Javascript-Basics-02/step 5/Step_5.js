75e803a12ad46df82ec305b9c88e73701807af04
Akram <akram.m.ahmad1989@gmail.com>
2019-12-16 20:57:12 +0200

    Step_5 Js2 Done

---
+++ Exercises/Javascript-Basics-02/step 5/Step_5.js
@@ -0,0 +1,11 @@
+function fun() {
+  if (confirm("Yes/No?")) {
+    document.getElementById("image1").style.display = "none";
+    document.getElementById("image2").style.display = "none";
+    document.getElementById("image3").style.display = "none";
+    document.getElementById("image4").style.display = "none";
+    document.getElementById("image5").style.display = "none";
+  } else {
+    alert("Love Codi");
+  }
+}
