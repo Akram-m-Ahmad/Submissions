e33dec4e602e251e050dda3f4e45b4d1c82deab3
Akram <akram.m.ahmad1989@gmail.com>
2019-12-16 13:45:14 +0200

    Step_4  Done

---
+++ Exercises/Javascript-Basics-01/Step_4.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  var name = document.getElementById("name").value;
+  var sureName = document.getElementById("surname").value;
+  var city = document.getElementById("city").value;
+
+  alert(
+    "name: " + name + "\n" + "sureName: " + sureName + "\n" + "city: " + city
+  );
+}
