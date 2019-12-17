6a30089e7eb3cedebc207a5c2cddfb3c9092ef03
Akram <akram.m.ahmad1989@gmail.com>
2019-12-17 11:45:14 +0200

    Step_2 Js3 Done

---
+++ Exercises/Javascript-Basics-03/step 2/Step_2.js
@@ -0,0 +1,8 @@
+document.getElementById("hide").addEventListener("click", function() {
+  var x = document.getElementById("text");
+  x.style.display = "none";
+});
+document.getElementById("show").addEventListener("click", function() {
+  var x = document.getElementById("text");
+  x.style.display = "block";
+});
