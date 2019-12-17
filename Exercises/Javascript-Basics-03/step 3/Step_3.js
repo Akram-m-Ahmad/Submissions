b0830d21865fba3fda713b53de9aab3caf17039c
Akram <akram.m.ahmad1989@gmail.com>
2019-12-17 12:03:11 +0200

    Step_3 Js3  Done

---
+++ Exercises/Javascript-Basics-03/step 3/Step_3.js
@@ -0,0 +1,9 @@
+document.getElementById("1").addEventListener("mouseover", function() {
+  document.getElementById("text").style.color = "green";
+});
+document.getElementById("2").addEventListener("mouseover", function() {
+  document.getElementById("text").style.color = "red";
+});
+document.getElementById("3").addEventListener("mouseover", function() {
+  document.getElementById("text").style.color = "blue";
+});
