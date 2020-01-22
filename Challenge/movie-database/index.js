ea53dba431d6074d5845567cc0730ba677afe3af
Akram <akram.m.ahmad1989@gmail.com>
2020-01-22 09:34:57 +0200

    Setp 3  Done!

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -2,13 +2,19 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("ok"));
+app.get("/", (req, res) => res.send("hello!"));
+
+app.get("/test", (req, res) => {
+  res.json({ status: 200, message: "ok" });
+});
+
+var today = new Date();
+var time = today.getHours() + ":" + today.getMinutes();
+
+app.get("/time", (req, res) => {
+  res.json({ status: 200, message: `${time}` });
+});
 
 app.listen(port, () =>
   console.log(`Server running at: http://localhost:${port}/`)
 );
-
-app.get("/quit", function(req, res) {
-  res.send("closing..");
-  app.close();
-});
